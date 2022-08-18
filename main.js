7e6f0eac45c99242ba448402ee5aae09e7c6842f
Jorge Osvany Benavides Nati <jbenavides@sistecredito.com>
2022-08-18 15:12:54 -0500

    first commit

--- main.js
+++ main.js
@@ -1,27 +1,46 @@
 function agregarProducto() {
   class Producto {
-    constructor(nombreProducto, precioProducto, cantProducto) {
+    constructor(nombreProducto, precioProducto, cantProducto,subtotal) {
       this._nombreProducto = nombreProducto;
       this._precioProducto = precioProducto;
       this._cantProducto = cantProducto;
+      this._subtotal = subtotal;
     }
   }
   let nombreProducto = document.getElementById("producto").value;
-  let precioProducto = document.getElementById("precio").value;
-  let cantProducto = document.getElementById("cantidad").value;
-  nuevoProducto = new Producto(nombreProducto, precioProducto, cantProducto);
-  subtotal = precioProducto*cantProducto;
+  let precioProducto = Number(document.getElementById("precio").value);
+  let cantProducto = Number(document.getElementById("cantidad").value);
+  let subtotal =precioProducto*cantProducto;
+
+  nuevoProducto = new Producto(nombreProducto, precioProducto, cantProducto,subtotal);
+
   agregar();
+  totalSubTotales()
 }
+
 
 var database = [];
 function agregar() {
-    
   database.push(nuevoProducto);
   document.getElementById("tabla").innerHTML += `<tbody>
                                                     <td>${nuevoProducto._nombreProducto}</td>
                                                     <td>$${nuevoProducto._precioProducto}</td>
                                                     <td>${nuevoProducto._cantProducto}</td>
-                                                    <td>$${subtotal}</td>
+                                                    <td>$${nuevoProducto._subtotal}</td>
                                                 </tbody>`;
+};
+console.log({database});
+
+ function totalSubTotales(){
+  var varTotalSubTotales = 0;
+  for(let sub of database){
+    varTotalSubTotales += sub._subtotal;
+    let iva = varTotalSubTotales*0.19;
+    let totalPagar = varTotalSubTotales+iva
+    console.log(sub);
+    console.log(varTotalSubTotales);
+    console.log(`el iva es ${iva}`);
+    console.log(`el total a pagar es ${totalPagar}`);
+    document.getElementById("subtotal").innerHTML = `SUBTOTAL = $${varTotalSubTotales} <br> IVA = $${iva} <br>  TOTAL A PAGAR = $${totalPagar}`;
+  }
 }
